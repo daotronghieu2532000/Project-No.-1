c5cc33abe9e7070481bf306472ab53c4d83e7df5
tronghieu2532000 <daotronghieu2532000@gmail.com>
2024-08-01 14:55:43 +0700

    In thêm dòng thông báo : Giỏ hàng của bạn chưa có sản phẩm phẩm cho người dùng hiểu họ đang gặp lỗi gì

--- javascip/duan1/view/cart/viewcart.php
+++ javascip/duan1/view/cart/viewcart.php
@@ -1,48 +1,45 @@
 <div class="row mb">
     <div class="boxtrai mr">
         <div class="row mb">
-        <div class="boxtitle">GIỎ HÀNG</div>
+            <div class="boxtitle">GIỎ HÀNG</div>
             <div class="row boxcontent cart">
                 <table>
-                    <?php 
-                        viewcart(1);
+                    <?php
+                    viewcart(1);
                     ?>
                 </table>
-            
             </div>
         </div>
-            
-        <div class="row mb bill">
-                    <?php 
-                            if(isset($_SESSION['user'])){
-                        ?>
-                            <a class="mr" href="index.php">
-                            <input style="padding: 10px;" type="button" value="Trang chủ">
-                            </a>
-                            <a class="mr" href="index.php?act=bill">
-                            <input style="padding: 10px;" type="button" value=" Đặt Mua">
-                            </a>
 
-                            <a href="index.php?act=delcart">
-                                <input style="padding: 10px;" type="button" value="Xóa sản phẩm">
-                            </a>
-                        </form>
-                        <?php
-                            }else{ 
-                        ?>
-                            <h1 style="color: red; text-align: center;">Vui lòng đăng nhập để tiếp tục đặt hàng!</h1>         
-                        <?php } ?>                   
+        <div class="row mb bill">
+            <?php
+            if (isset($_SESSION['user'])) {
+            ?>
+                <a class="mr" href="index.php">
+                    <input style="padding: 10px;" type="button" value="Trang chủ">
+                </a>
+                <a class="mr" href="index.php?act=bill">
+                    <input style="padding: 10px;" type="button" value="Đặt Mua">
+                </a>
+                <a href="index.php?act=delcart">
+                    <input style="padding: 10px;" type="button" value="Xóa sản phẩm">
+                </a>
+                
+            <?php
+            } else {
+            ?>
+                <h1 style="color: red; text-align: center;">Vui lòng đăng nhập để tiếp tục đặt hàng!</h1>
+            <?php } ?>
         </div>
-        
+
+        <?php if (isset($thongbao) && $thongbao != "") : ?>
+            <div class="alert alert-danger">
+                <?php echo $thongbao; ?>
+            </div>
+        <?php endif; ?>
+
     </div>
     <div class="boxphai">
-            <?php include "view/boxright.php";?>
-        </div>
-    <script>
-
-    </script>
+        <?php include "view/boxright.php"; ?>
+    </div>
 </div>
-
-
-
-
