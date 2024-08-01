1d5e75bd34c319cde481d110a011e92a06f89e8a
tronghieu2532000 <daotronghieu2532000@gmail.com>
2024-08-01 14:32:00 +0700

    Đã sửa lỗi chưa hiển thị thông báo thành công khi edit thông tin

--- javascip/duan1/view/taikhoan/edit_taikhoan.php
+++ javascip/duan1/view/taikhoan/edit_taikhoan.php
@@ -1,59 +1,53 @@
 <div class="row mb">
-<div class="boxtrai mr">  
-    <div class="row mb">
-           
+    <div class="boxtrai mr">
+        <div class="row mb">
             <div class="boxtitle">
                 <h1 class="spct text-size">THÔNG TIN CÁ NHÂN</h1>
             </div>
             <div class="boxcontent row formtaikhoan">
+                <?php
+                if (isset($_SESSION['user']) && is_array($_SESSION['user'])) {
+                    extract($_SESSION['user']);
+                }
 
-                <?php 
-                    if(isset($_SESSION['user']) && (is_array($_SESSION['user']))){
-                        extract($_SESSION['user']);
-                    }
+                if (isset($_SESSION['thongbao'])) {
+                    echo "<h2 class='thongbao'>{$_SESSION['thongbao']}</h2>";
+                    unset($_SESSION['thongbao']); 
+                }
+                // Sau khi cập nhâp thành công, thông báo thành  công cho người dùng biết : OK xong !
                 ?>
-
                 <form action="index.php?act=edit_taikhoan" method="post">
                     <div class="row mb">
                         Email
-                        <input type="email" name="email" id="email" value="<?=$email?>" required>
+                        <input type="email" name="email" id="email" value="<?= $email ?>" required>
                     </div>
                     <div class="row mb">
                         Username
-                        <input type="text" name="user" value="<?=$user?>" required>
+                        <input type="text" name="user" value="<?= $user ?>" required>
                     </div>
                     <div class="row mb">
                         Password
-                        <input type="text" name="pass" value="<?=$pass?>" required>
+                        <input type="text" name="pass" value="<?= $pass ?>" required>
                     </div>
                     <div class="row mb">
                         Địa chỉ
-                        <input type="text" name="address" value="<?=$address?>" required>
+                        <input type="text" name="address" value="<?= $address ?>" required>
                     </div>
                     <div class="row mb">
                         Số điện thoại
-                        <input type="text" name="tel" value="<?=$tel?>" required>
+                        <input type="text" name="tel" value="<?= $tel ?>" required>
                     </div>
                     <div class="row mb">
-                        <input type="hidden" name="id" value="<?=$id?>">
+                        <input type="hidden" name="id" value="<?= $id ?>">
                         <input type="submit" value="Cập nhật" name="capnhat">
                         <input type="reset" value="Nhập lại">
                     </div>
                 </form>
-                <h2 class="thongbao">
-                    <?php 
-                        if(isset($thongbao) && ($thongbao != "")){
-                            echo $thongbao;
-                        }
-                    ?>
-                </h2>
             </div>
         </div>
-        
-        
     </div>
 
     <div class="boxphai">
-        <?php include "view/boxright.php"?>;
+        <?php include "view/boxright.php"; ?>
     </div>
 </div>
