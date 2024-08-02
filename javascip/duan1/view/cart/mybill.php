5b8cc1dd1f7e56fef8eb7cbf9f6c888742d61d9e
tronghieu2532000 <daotronghieu2532000@gmail.com>
2024-08-02 23:10:27 +0700

    Thêm tổng tiền trong , hủy đơn hàng cho khách

--- javascip/duan1/view/cart/mybill.php
+++ javascip/duan1/view/cart/mybill.php
@@ -10,14 +10,18 @@
                         <th>SỐ LƯỢNG MẶT HÀNG</th>
                         <th>TỔNG GIÁ TRỊ ĐƠN HÀNG</th>
                         <th>TÌNH TRẠNG ĐƠN HÀNG</th>
+                        <th>THAO TÁC</th>
                     </tr>
+                    
                     <?php 
+                        $total_amount = 0;
                         if(is_array($mybill)){
-                            
+                            $thaotac = "Hủy đơn hàng";
                             foreach ($mybill as $bill) {
                                 extract($bill);
                                 $ttdh = get_ttdh($bill['bill_status']);
                                 $countsp = loadall_cart_count($bill['id']);
+                                $total_amount += $bill['total'];
                                 echo '  
                                     <tr>
                                         <td>'.$bill['id'].'</td>
@@ -25,6 +29,7 @@
                                         <td>'.$countsp.'</td>
                                         <td>'.$bill['total'].'</td>
                                         <td>'.$ttdh.'</td>
+                                        <td>'.$thaotac.'</td>
                                     </tr>
                                 ';
                             }
@@ -32,6 +37,8 @@
                     ?>
                     
                 </table>
+                <h2 style="margin-top: 10px;">THỰC TÍNH : <?php echo $total_amount; ?> vnđ. </h2>
+                <h3>Bạn hãy chuẩn bị số tiền trên để có thể thanh toán tổng đơn hàng ! Xin cảm ơn.</h3>
             </div>
         </div>
     </div>
@@ -40,7 +47,3 @@
         </div>
     
 </div>
-
-
-
-
